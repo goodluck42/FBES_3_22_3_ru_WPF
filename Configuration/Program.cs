91c3763ef83c13960235b32c56240930ea6b159c
xrlogic1 <xrlogic1@gmail.com>
2023-11-06 20:25:37 +0400

    *

---
+++ Configuration/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration.Memory;
+using Microsoft.Extensions.Configuration.Ini;
+
+
+var builder = new ConfigurationBuilder();
+
+
+builder
+    .AddJsonFile("config.json")
+    .AddIniFile("config.ini")
+    .AddInMemoryCollection(new List<KeyValuePair<string, string?>>
+    {
+        new KeyValuePair<string, string?>("ProductsFileName", "products.json"),
+    });
+
+
+var configuration = builder.Build();
+
+Console.WriteLine(configuration["ApiKey"]);
+Console.WriteLine(configuration["ProductsFileName"]);
+Console.WriteLine(configuration["Settings:HotKey1"]);
