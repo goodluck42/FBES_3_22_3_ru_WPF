3b9e5352b58910d99358e4de45ed623b1925748d
xrlogic1 <xrlogic1@gmail.com>
2023-11-06 21:03:46 +0400

    *

---
+++ Configuration/Manager.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Configuration;
+
+public class Manager
+{
+    private readonly IConfiguration _configuration;
+
+    public Manager(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void TestMethod()
+    {
+        Console.WriteLine(_configuration["ApiKey"]);
+    }
+}
--- Configuration/Program.cs
+++ Configuration/Program.cs
@@ -1,11 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.Configuration.Memory;
 using Microsoft.Extensions.Configuration.Ini;
 
-
+var serviceCollection = new ServiceCollection();
 var builder = new ConfigurationBuilder();
 
 // DO NOT COMMIT CONFIG FILES!
@@ -20,6 +22,17 @@ builder
 
 var configuration = builder.Build();
 
-Console.WriteLine(configuration["ApiKey"]);
-Console.WriteLine(configuration["ProductsFileName"]);
-Console.WriteLine(configuration["Settings:HotKey1"]);
+serviceCollection.AddSingleton<IConfiguration>(provider => configuration);
+serviceCollection.AddTransient<Manager>();
+
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var manager = serviceProvider.GetService<Manager>()!;
+
+manager.TestMethod();
+
+
+// Console.WriteLine(configuration["ApiKey"]);
+// Console.WriteLine(configuration["ProductsFileName"]);
+// Console.WriteLine(configuration["Settings:HotKey1"]);
